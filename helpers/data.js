d463fb1b057f334963f53ce48295b96bf39472b9
Leslaw Kula <it@kula.wroclaw.pl>
2024-10-08 12:41:18 +0200

    add tests

---
+++ helpers/data.js
@@ -0,0 +1,6 @@
+import 'dotenv/config'
+
+export const userId = 'd48279cf-86f7-4b6a-a308-d262254f3301'
+export const baseUrl = 'https://demoqa.com';
+export const userName = 'leszekkk';
+export const password = process.env.SECRET_PASSWORD;
